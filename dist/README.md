63349daba39a9d22a8b94fbd45e58d171f4d5666
desguerra <ddesguerra@ucdavis.edu>
2022-08-25 14:13:54 -0700

    generate markdown from user input

--- dist/README.md
+++ dist/README.md
@@ -1,3 +1,3 @@
-{
-	"title": "testing"
-}
+# testing again
+
+    
--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const inquirer = require('inquirer');
 
 const fs = require('fs');
 
-// const generatemarkDown = require('./utils/generateMarkdown.js');
+const generateMarkdown = require('./utils/generateMarkdown.js');
 
 
 // TODO: Create an array of questions for user input
@@ -51,7 +51,7 @@ function init() {
     .then(data => {
         console.log(data);
 
-        writeToFile('./dist/README.md', JSON.stringify(data, null, '\t'));
+        writeToFile('./dist/README.md', generateMarkdown(data));
     });
 };
 
