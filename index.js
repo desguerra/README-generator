f75d238f2d2b62a413995a4632595e8f2bdb57b6
desguerra <ddesguerra@ucdavis.edu>
2022-08-25 13:36:05 -0700

    generate README with user input in dist directory

--- index.js
+++ index.js
@@ -1,14 +1,34 @@
-// TODO: Include packages needed for this application
+// packages needed for this application
 const inquirer = require('inquirer');
 
-// const fs = require('fs'); // TODO: add to generateMarkdown.js file, then include that file here?
+const fs = require('fs');
+
+// const generatemarkDown = require('./utils/generateMarkdown.js');
+
 
 // TODO: Create an array of questions for user input
 const questions = [];
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {
+// function to write README file
+const writeToFile = (fileName, data) => {
 
+    return new Promise((resolve, reject) => {
+        fs.writeFile(fileName, data, err => {
+            // if there's an error, reject the Promise and send the error to the Promise's `.catch()` method
+            if (err) {
+                reject(err);
+                // return out of the function here to make sure the Promise doesn't accidentally execute the resolve() function as well
+                return;
+            }
+      
+            // if everything went well, resolve the Promise and send the successful data to the `.then()` method
+            resolve({
+                ok: true,
+                message: 'File created!'
+            });
+        });
+    });
+    
 };
 
 // TODO: Create a function to initialize app
@@ -30,6 +50,8 @@ function init() {
     ])
     .then(data => {
         console.log(data);
+
+        writeToFile('./dist/README.md', JSON.stringify(data, null, '\t'));
     });
 };
 
