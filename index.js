c6bad682504c7238b258ebdffff90b8efd536ef4
desguerra <ddesguerra@ucdavis.edu>
2022-08-25 13:19:07 -0700

    update user input branch

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
-const fs = require('fs');
+
+// const fs = require('fs'); // TODO: add to generateMarkdown.js file, then include that file here?
 
 // TODO: Create an array of questions for user input
 const questions = [];
